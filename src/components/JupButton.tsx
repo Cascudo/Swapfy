67edf45cee8bf8d9e714e2796ca118b4d14dfeb2
Cascudo <prado_celso@hotmail.com>
2025-01-08 00:32:07 -0300

    UI Inprovments
    UI Inprovments

--- src/components/JupButton.tsx
+++ src/components/JupButton.tsx
@@ -29,23 +29,28 @@ const JupButton = React.forwardRef(
         return 'p-5 text-md !font-[Inter] font-semibold !text-opacity-100';
       }
     })();
-    const background = bgClass || 'text-white bg-[#191B1F] dark:bg-black/50';
+    
+    // Special handling for gradient buttons (like Swap)
+    const isGradientButton = highlighted || children === 'Swap';
+    const background = bgClass || (isGradientButton ? 'text-white bg-[#191B1F]' : 'text-white bg-[#191B1F] dark:bg-black/50');
+    
     return (
       <button
         type={type}
         ref={ref}
         className={classNames({
-          'relative text-current [text-rendering:optimizeLegibility] [-webkit-font-smoothing:antialiased] [-moz-osx-font-smoothing:grayscale]': true,
-          'jup-gradient': highlighted,
+          'relative text-current [text-rendering:optimizeLegibility] [-webkit-font-smoothing:antialiased] [-moz-osx-font-smoothing:grayscale] hover:opacity-80': true,
+          'jup-gradient before:!opacity-100': isGradientButton,
           'opacity-50 cursor-not-allowed': disabled,
           [background]: true,
           [className]: true,
           [rounded || 'rounded-xl']: true,
+          '!text-white': true,
         })}
         disabled={disabled}
         onClick={onClick}
       >
-        <div className={`${contentClass} h-full w-full leading-none !text-inherit`}>{children}</div>
+        <div className={`${contentClass} h-full w-full leading-none !text-white`}>{children}</div>
       </button>
     );
   },
